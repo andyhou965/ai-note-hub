9ecdb08b4f33c445d8b143941dad9751d04e297e
AndyHou <andyhou965@gmail.com>
2024-06-10 13:28:42 +0400

    finish the Vector Embedding

---
+++ src/lib/db/pinecone.ts
@@ -0,0 +1,13 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+
+const apiKey = process.env.PINECONE_API_KEY;
+
+if (!apiKey) {
+  throw Error("PINECONE_API_KEY is not set");
+}
+
+const pinecone = new Pinecone({
+  apiKey: apiKey,
+});
+
+export const notesIndex = pinecone.index("ai-note-app");
